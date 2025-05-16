2f039b4f88498683f0ca6f4948ce210ca8e3d527
Jkivuvu <149970979+Jkivuvu@users.noreply.github.com>
2025-05-16 09:59:46 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, send_from_directory
+import datetime
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    current_time = datetime.datetime.now().year
+    return render_template('index.html', year=current_time)
 
 @app.route('/download/<filename>')
 def download_file(filename):
