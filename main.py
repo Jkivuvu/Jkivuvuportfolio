97f316beaa0ff46067888ec2e671561b319e667f
Jkivuvu <149970979+Jkivuvu@users.noreply.github.com>
2025-05-16 09:30:36 -0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
@@ -6,5 +6,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('files', filename, as_attachment=True)
 if __name__ == "__main__":
     app.run(debug=True)
